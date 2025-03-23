274f2f57139f4bb375cc806660764796a9b395ab
Louay-Trabelsi <louayt250@gmail.com>
2025-03-23 04:51:21 +0100

    add to cart button

--- client/src/components/Oneproduct.jsx
+++ client/src/components/Oneproduct.jsx
@@ -1,22 +1,44 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import '../css/OneProduct.css'
+import axios from 'axios'
 
 const OneProduct = ({ el, i, setprod }) => {
   const navigate = useNavigate()
   console.log(el.image[1]);
 
+  const token=localStorage.getItem("user")
+
 
   return (
     <div className="product-item">
-      <img src={el.image[0]} alt={el.name} />
-      <div className="content">
-        <h3>{el.name}</h3>
-        <p>{el.description}</p>
-        <div className="price">${el.price}</div>
-        <button onClick={() => {
+      <img onClick={() => {
           setprod(el)
           navigate(`/products/${i}`)
+        }} src={el.image[0]} alt={el.name} />
+      <div className="content">
+        <h3 onClick={() => {
+          setprod(el)
+          navigate(`/products/${i}`)
+        }}>{el.name}</h3>
+        <p>{el.description}</p>
+        <div className="price">${el.price}</div>
+        <button onClick={async() => {
+          if(el.role==="game"){
+            await axios.post(`http://localhost:208/api/cart`,{gameId:el.id},{
+              headers: {
+                Authorization: `Bearer ${token}`
+              }
+            })
+          }
+          if(el.role==="electronic"){
+            await axios.post(`http://localhost:208/api/cart`,{ElectronicsId:el.id},{
+              headers: {
+                Authorization: `Bearer ${token}`
+              }
+            })
+          }
+          
         }}>
           View Details
         </button>
